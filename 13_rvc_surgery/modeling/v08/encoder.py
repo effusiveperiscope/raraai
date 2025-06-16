f71f5d10365710c6e0221685f9a7f01d713f02f4
Anonymous <>
2025-06-16 13:23:58 -0700

    13 - v08 - modifiable grl

--- 13_rvc_surgery/modeling/v08/encoder.py
+++ 13_rvc_surgery/modeling/v08/encoder.py
@@ -134,8 +134,8 @@ class GradientReversal(torch.nn.Module):
 class SpeakerClassifier(nn.Module):
     def __init__(self, embed_dim, spk_dim):
         super(SpeakerClassifier, self).__init__()
+        self.grl = GradientReversal(lambda_reversal=1),
         self.classifier = nn.Sequential(
-            GradientReversal(lambda_reversal=1),
             weight_norm(nn.Conv1d(embed_dim, embed_dim, kernel_size=5, padding=2)),
             nn.ReLU(),
             weight_norm(nn.Conv1d(embed_dim, embed_dim, kernel_size=5, padding=2)),
@@ -148,6 +148,7 @@ class SpeakerClassifier(nn.Module):
             x = (B, embed_dim, len)
         '''
         # pass through classifier
-        outputs = self.classifier(x, override_lambda)  # (B, nb_speakers)
+        outputs = self.classifier(self.grl(
+            x, override_lambda=override_lambda))  # (B, nb_speakers)
         outputs = torch.mean(outputs, dim=-1)
         return outputs
