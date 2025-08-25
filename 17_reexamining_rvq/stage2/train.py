fix broken gradient bug

17_reexamining_rvq/stage2/train.py
@@ -304,9 +304,10 @@ class TrainingModule(pl.LightningModule):
             y_d_hat_r, y_d_hat_g, _, _ = self.net_d(audio, fake_audio.detach())
             loss_d, _, _ = discriminator_loss(y_d_hat_r, y_d_hat_g)
 
+            f0_fake_disc = self.f0_disc(f0_pred.unsqueeze(-1).detach())
             f0_real_disc = self.f0_disc(target.unsqueeze(-1))
             f0_real_loss = torch.mean((1 - f0_real_disc) ** 2)
-            f0_fake_loss = torch.mean(f0_fake_disc.detach() ** 2)
+            f0_fake_loss = torch.mean(f0_fake_disc ** 2)
             f0_disc_loss = f0_real_loss + f0_fake_loss
             loss_d = loss_d + f0_disc_loss * hp.train.c_f0_adv
 

17_reexamining_rvq/stage2/train_f0_base.py
@@ -94,9 +94,10 @@ class TrainingModule(pl.LightningModule):
         else:
             g_norm = None
 
+        f0_fake_disc = self.net_d(f0_pred.unsqueeze(-1).detach())[vuv_mask]
         f0_real_disc = self.net_d(target.unsqueeze(-1))[vuv_mask]
         f0_real_loss = torch.mean((disc_label - f0_real_disc) ** 2)
-        f0_fake_loss = torch.mean(f0_fake_disc.detach() ** 2)
+        f0_fake_loss = torch.mean(f0_fake_disc ** 2)
         disc_loss = f0_real_loss + f0_fake_loss
 
         if disc_loss.requires_grad: